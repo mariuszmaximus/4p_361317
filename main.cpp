c6bed607cadea87f7abe0e9a41317b1bc26cd76d
mariuszmaximus <mariusz.maximus@gmail.com>
2022-06-02 11:57:25 +0200

    up

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
+#include "gtest/gtest.h"
+
+#include "pthread.h"
+#include "sched.h"
+#include "semaphore.h"
+
 int main(int, char**) {
     std::cout << "Hello, world!\n";
 }
